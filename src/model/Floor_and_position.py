b733231a968ba803eb14f60c3df8657a124bec1e
Uladzimir Shopik <wlodzimierz.szopik@gmail.com>
2025-05-17 01:29:54 +0200

    use data structure in console menu

--- src/model/Floor_and_position.py
+++ src/model/Floor_and_position.py
@@ -1,3 +1,6 @@
+from src.view.ConsoleCoworkingApp import ConsoleCoworkingApp
+
+
 def get_floor_seat_data(floor, number_of_seats):
     """
     Tą funkcją tworzymy listę rezerwacji miejsc na danym piętrze.
@@ -7,15 +10,15 @@ def get_floor_seat_data(floor, number_of_seats):
         # Jeśli numer piętra nie zgadza się wyświetli bład i  stopujemy program.
         raise ValueError('Invalid floor number.  Please use 4, 5, 6, or 7.')
 
-    # Tworzymy liste któa reprezentuje miejsc dostępnych.
-    # Zaczynamy od miejsc dostępnych, więc lista zawiera numery miejsc
-    available_seats = []
+    # Tworzymy liste która reprezentuje miejsca.
+    # Zaczynamy od miejsc dostępnych, więc lista zawiera numery miejsc i status rezerwacji = False
+    seats = []
 
     for seat_number in range(1, number_of_seats + 1):  # pętla od 1 do numeru miejsca
-        available_seats.append(seat_number)
+        seats.append([seat_number, False])
 
     # Zwracamy terraZ listę która ma w sobienumer piętra i listę dostępnych miejsc
-    return [floor, available_seats]
+    return [floor, seats]
 
 
 
@@ -54,19 +57,22 @@ def main_app():
     # Struktura danych zawierajaca rezerwacje miejsc na wsyztskich piętrach
     all_seats_data = create_all_floors_data()
 
+    # Wywołanie klasy ConsoleCoworkingApp z danymi o miejscach
+    app = ConsoleCoworkingApp(all_seats_data)
+    app.start()
     # Całą struktura w senise jej wydrukownaie
-    print("Pełna struktura :")
-    print(all_seats_data)
-
-    # Informacje o miejscach dla każdego piętra
-    print("4th floor:")
-    print(all_seats_data[0])
-    print("5th floor:")
-    print(all_seats_data[1])
-    print("6th floor:")
-    print(all_seats_data[2])
-    print("7th floor:")
-    print(all_seats_data[3])
+    # print("Pełna struktura :")
+    # print(all_seats_data)
+    #
+    # # Informacje o miejscach dla każdego piętra
+    # print("4th floor:")
+    # print(all_seats_data[0])
+    # print("5th floor:")
+    # print(all_seats_data[1])
+    # print("6th floor:")
+    # print(all_seats_data[2])
+    # print("7th floor:")
+    # print(all_seats_data[3])
 
 
 if __name__ == "__main__":
--- src/view/ConsoleCoworkingApp.py
+++ src/view/ConsoleCoworkingApp.py
@@ -1,38 +1,59 @@
 import sys
+from typing import List, Tuple
 
+# Consolowe przedstawienie aplikacji.
 class ConsoleCoworkingApp:
+    def __init__(self, data: Tuple[int, List[Tuple[int, bool]]]):
+        self.data = data
 
     def print_menu(self):
-        print("1. View available desks")
-        print("2. Book a desk")
-        print("3. Cancel booking")
-        print("4. Exit")
+        """
+        Tą funkcją wyświetlamy menu w konsoli.
+        """
+        print("1. Pokaż dostępne miejsca")
+        print("2. Rezerwuj miejsce")
+        print("3. Anuluj rezerwację")
+        print("4. Wyjdź")
 
     def handle_menu_option(self, option):
+        """
+        Tą funkcją obsługujemy wybraną opcję z menu.
+        """
         if option == "1":
-            print("View available desks...")
+            self.print_data()
         elif option == "2":
-            print("Book a desk...")
+            print("Rezerwuj miejsce...")
         elif option == "3":
-            print("Cancel booking...")
+            print("Anuluj rezerwację...")
         elif option == "4":
-            print("Exit.")
+            print("Wyjdź.")
             sys.exit(0)
         else:
-            print("Wrong option.")
+            print("Niepoprawny wybór. Proszę spróbować ponownie.")
+
+    def print_data(self):
+        """
+        Tą funkcją wyświetlamy dostępne miejsca na każdym piętrze.
+        """
+        for floor, seats in self.data:
+            print(f"Piętro {floor}:" , end=" ")
+            for seat, reserved in seats:
+                print(f"{seat}({'X' if reserved else 'O'})" , end=" ")
+            print("")
 
     def start(self):
-        print("Welcome to the Coworking Application!")
+        """
+        Tą funkcją uruchamiamy aplikację.
+        """
+        print("Witaj w aplikacji do rezerwacji miejsc!")
         while True:
             self.print_menu()
-            selected_option = input("Please select an option (1-4): ")
+            selected_option = input("Wybierz opcję (1-4): ")
             self.handle_menu_option(selected_option)
-
-            is_continue = input("Do you want to continue (y/n): ")
-            if is_continue.strip().lower() != "y":
+            is_continue = input("Czy chcesz kontynuować? (t/n): ")
+            if is_continue.strip().lower() != "t":
                 break
 
-
 if __name__ == "__main__":
-    app = ConsoleCoworkingApp()
+    app = ConsoleCoworkingApp([])
     app.start()
