18689fa591e4895ac85aa8721a0e081975aac685
Adrian Grabowski <adik.grabowski@mbp-adrian.home>
2025-05-16 20:55:03 +0200

    Struktura danych

---
+++ src/model/Floor_and_position.py
@@ -0,0 +1,73 @@
+def get_floor_seat_data(floor, number_of_seats):
+    """
+    Tą funkcją tworzymy listę rezerwacji miejsc na danym piętrze.
+    """
+    # Sprawdzenie czy numer piętra jest prawidłowy. Zwracanie listy zawierającej numer piętra i liczba miejsc
+    if floor not in (4, 5, 6, 7):
+        # Jeśli numer piętra nie zgadza się wyświetli bład i  stopujemy program.
+        raise ValueError('Invalid floor number.  Please use 4, 5, 6, or 7.')
+
+    # Tworzymy liste któa reprezentuje miejsc dostępnych.
+    # Zaczynamy od miejsc dostępnych, więc lista zawiera numery miejsc
+    available_seats = []
+
+    for seat_number in range(1, number_of_seats + 1):  # pętla od 1 do numeru miejsca
+        available_seats.append(seat_number)
+
+    # Zwracamy terraZ listę która ma w sobienumer piętra i listę dostępnych miejsc
+    return [floor, available_seats]
+
+
+
+def create_all_floors_data():
+    """
+    Funkcja ta tworzy głowene informacje o wszytskich piętrach i ich miejscach
+
+    Używa funkcji get_floor_seat_data żeby uzyskać dane dla każdego piętra
+    Zwraca listę list gdzie każda wewnetrzna lista reprezentuje piętro i jego miejsca
+    """
+    # Zdefiniowanie numery pięter i liczbę miejsc na każdym piętrze
+    floor_numbers = [4, 5, 6, 7]
+    number_of_seats_per_floor = [20, 30, 25, 35]
+
+    # Tworzymy pustą listę aby przechować dane dla wszystkich pięter
+    all_floors_data = []
+
+    # Pętla któa przechodzi przez każde piętro i miejsce
+    for i in range(len(floor_numbers)):
+        floor = floor_numbers[i]
+        seats = number_of_seats_per_floor[i]
+        #Pobranie info o miejscach na bieżącym piętrze
+        floor_data = get_floor_seat_data(floor, seats)
+        # Dodanie danych dla tego pietra do listy wszystkich pięter
+        all_floors_data.append(floor_data)
+
+    # Zwrócenie pełnej struktury danych
+    return all_floors_data
+
+
+
+def main_app():
+    """
+   Główna funkcja w której program działa w sensie np wywołuje inne funckje - można a nawet chyba trzeba to zrobić w osobnym Pliku - Michał się wypowie
+    """
+    # Struktura danych zawierajaca rezerwacje miejsc na wsyztskich piętrach
+    all_seats_data = create_all_floors_data()
+
+    # Całą struktura w senise jej wydrukownaie
+    print("Pełna struktura :")
+    print(all_seats_data)
+
+    # Informacje o miejscach dla każdego piętra
+    print("4th floor:")
+    print(all_seats_data[0])
+    print("5th floor:")
+    print(all_seats_data[1])
+    print("6th floor:")
+    print(all_seats_data[2])
+    print("7th floor:")
+    print(all_seats_data[3])
+
+
+if __name__ == "__main__":
+    main_app()
