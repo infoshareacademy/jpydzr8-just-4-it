92f77c83057d14ea6222e6369e70f58de36ca986
Uladzimir Shopik <wlodzimierz.szopik@gmail.com>
2025-06-18 13:26:45 +0200

    add registration, login, logout options

--- src/model/coworking_model.py
+++ src/model/coworking_model.py
@@ -1,7 +1,26 @@
 
 class Coworking:
-    def __init__(self, floors):
+    def __init__(self, floors, users):
         self.floors = floors
+        self.users = users
+        self.current_user = None
+
+    def login_user(self,login):
+        for user in self.users:
+            if user.email == login or user.phone == login:
+                self.current_user = user
+                return user
+        return None
+
+    def logout_user(self):
+        self.current_user = None
+
+    def register_user(self, user):
+        self.users.append(user)
+        self.current_user = user
+
+    def is_current_user_exists(self):
+        return self.current_user is not None
 
     def reserve_seat(self, floor_number, seat_number):
         return False
@@ -14,7 +33,6 @@ class Coworking:
             if floor.number == floor_number:
                 return floor
         return None
-
 
 class Floor:
     def __init__(self, number, number_of_seats):
@@ -31,7 +49,12 @@ class Seat:
         self.number = number
         self.reserved = reserved
 
-
+class User:
+    def __init__(self, first_name: object, last_name: object, email: object, phone: object) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone = phone
 
 
 
